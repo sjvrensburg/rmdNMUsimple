33f21e62103c063eca984175d84e030fae0e2c01
sjvrensburg <stefanj@mandela.ac.za>
2025-07-23 11:33:50 +0200

    Several fixes to allow bookdown cross-referencing and an improved/working assessment template.

--- R/outputs.R
+++ R/outputs.R
@@ -22,7 +22,7 @@ nmu_beamer <- function(...) {
 #' @param ... Additional arguments to pdf_document
 #' @export
 nmu_document <- function(...) {
-  rmarkdown::pdf_document(
+  bookdown::pdf_document2(
     template = system.file("rmarkdown/templates/document/resources/template.tex", package = "rmdNMUsimple"),
     pandoc_args = c(
       "--lua-filter", system.file("rmarkdown/templates/document/resources/boxes.lua", package = "rmdNMUsimple")
@@ -38,23 +38,25 @@ nmu_document <- function(...) {
 #' @param ... Additional arguments to pdf_document
 #' @export
 nmu_assessment <- function(solutions = FALSE, ...) {
-  pandoc_args <- NULL
-  if (solutions) {
-    pandoc_args <- c("--variable", "solutions=true")
+  # Initialise pandoc arguments vector
+  pandoc_args <- character(0)
+
+  # Conditionally define solutions variable for pandoc processing
+  if (isTRUE(solutions)) {
+    pandoc_args <- c(pandoc_args, "--variable", "solutions=true")
   }
 
-  # Get the additional arguments
+  # Process additional function arguments
   args <- list(...)
 
-  # Handle addenda file if specified
+  # Handle addenda file specification with path normalisation
   if ("addenda" %in% names(args)) {
-    # Convert to absolute path to ensure it's found during compilation
     addenda_path <- normalizePath(args$addenda, mustWork = FALSE)
     pandoc_args <- c(pandoc_args, "--variable", paste0("addenda=", addenda_path))
-    # Remove from args so it's not passed twice
     args$addenda <- NULL
   }
 
+  # Construct pdf_document with automated mark calculation
   do.call(rmarkdown::pdf_document, c(
     list(
       template = system.file("rmarkdown/templates/assessment/resources/template.tex",
