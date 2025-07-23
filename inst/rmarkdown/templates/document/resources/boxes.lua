52060de471c776104f46fa42ecf2df6487a1a84c
sjvrensburg <stefanj@mandela.ac.za>
2025-07-23 14:33:41 +0200

    Basic enhancements to the document template

--- inst/rmarkdown/templates/document/resources/boxes.lua
+++ inst/rmarkdown/templates/document/resources/boxes.lua
@@ -2,7 +2,7 @@ function Div (div)
   -- Extract the custom title from attributes if it exists
   local custom_title = div.attributes.name
 
-  if div.classes:includes('note') then
+  if div.classes:includes('notebox') then
     local begin_env = '\\begin{notebox}'
     if custom_title then
       begin_env = '\\begin{notebox}[' .. custom_title .. ']'
@@ -11,7 +11,7 @@ function Div (div)
            div.content ..
            pandoc.List { pandoc.RawBlock('latex', '\\end{notebox}') }
 
-  elseif div.classes:includes('warning') then
+  elseif div.classes:includes('warningbox') then
     local begin_env = '\\begin{warningbox}'
     if custom_title then
       begin_env = '\\begin{warningbox}[' .. custom_title .. ']'
@@ -20,7 +20,25 @@ function Div (div)
            div.content ..
            pandoc.List { pandoc.RawBlock('latex', '\\end{warningbox}') }
 
-  elseif div.classes:includes('example') then
+  elseif div.classes:includes('importantbox') then
+    local begin_env = '\\begin{warningbox}'
+    if custom_title then
+      begin_env = '\\begin{warningbox}[' .. custom_title .. ']'
+    end
+    return pandoc.List { pandoc.RawBlock('latex', begin_env) } ..
+           div.content ..
+           pandoc.List { pandoc.RawBlock('latex', '\\end{warningbox}') }
+
+  elseif div.classes:includes('ideabox') then
+    local begin_env = '\\begin{ideabox}'
+    if custom_title then
+      begin_env = '\\begin{ideabox}[' .. custom_title .. ']'
+    end
+    return pandoc.List { pandoc.RawBlock('latex', begin_env) } ..
+           div.content ..
+           pandoc.List { pandoc.RawBlock('latex', '\\end{ideabox}') }
+
+  elseif div.classes:includes('examplebox') then
     local begin_env = '\\begin{examplebox}'
     if custom_title then
       begin_env = '\\begin{examplebox}[' .. custom_title .. ']'
